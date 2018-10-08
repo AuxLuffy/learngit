81e7318aa61867b6d15f0e3c378edf74f7a8bb3d
sunzhangfei <sunzhangfei@zuoyebang.com>
2018-10-08 18:33:23 +0800

    edit Student.java

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student{
+	public int age;
+	public int sex;
+	public String name;
+	public Student(String name, int sex, int age){
+		this.name = name;
+		this.sex = sex;
+		this.age = age;
+	}
+	public void setAge(int newAge){
+		this.age = newAge;
+	}
+}
